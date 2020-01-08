407a21dac074cb5f1d5256a66acdb0d026b8dda7
debian <flawxmusic@gmail.com>
2020-01-08 15:37:35 +0100

    initial

---
+++ app/views/arts/_art.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! art, :id, :author, :title, :description, :date, :photo, :created_at, :updated_at
+json.url art_url(art, format: :json)
---
+++ db/migrate/20200108103451_create_arts.rb
@@ -0,0 +1,13 @@
+class CreateArts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :arts do |t|
+      t.string :author
+      t.string :title
+      t.text :description
+      t.string :date
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
